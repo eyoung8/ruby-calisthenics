61e0c9eea2ed4aca2740570599901148e952eaee
Evan Young <eyoung8@binghamton.edu>
2016-02-08 23:32:03 +0000

    Finished part 1 and removed pending from spec file

--- lib/fun_with_strings.rb
+++ lib/fun_with_strings.rb
@@ -1,12 +1,36 @@
 module FunWithStrings
   def palindrome?
-    # your code here
+    sanitized = self.downcase.gsub(/\W/,'')
+    return sanitized == sanitized.reverse
   end
+  
   def count_words
-    # your code here
+    sanitized = self.downcase.gsub(/\W/,' ')
+    word_count_hash = Hash.new
+    sanitized.split.each do |word|
+      word_count_hash[word] ||= 0
+      word_count_hash[word] += 1
+    end
+    return word_count_hash
   end
+  
+  def sort
+    return self.split("").sort.join
+  end
+  
   def anagram_groups
-    # your code here
+    sanitized = self.downcase.gsub(/\W/,' ')
+    anagram_groups_hash = Hash.new
+    sanitized.split.each do |word|
+      sorted_word = word.sort
+      anagram_groups_hash[sorted_word] ||= Array.new
+      anagram_groups_hash[sorted_word] << word
+    end
+    anagram_groups_array = Array.new
+    anagram_groups_hash.each do |group, value|
+      anagram_groups_array << value
+    end
+    return anagram_groups_array
   end
 end
 
